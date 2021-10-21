4b6dd91b70ff02374e02a16ab32c1290b17b6395
reveriess <sayid159@gmail.com>
2021-10-21 13:20:36 +0700

    commit on update

--- personalwebsite/home/models.py
+++ personalwebsite/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StoryPost(models.Model):
+    text=models.TextField()
+    date=models.DateTimeField()
+    title=models.CharField(max_length=40)
+    summary=models.TextField()
+
+    class Meta:
+        ordering=['date']
+
+    def __str__(self):
+        return self.title
--- personalwebsite/home/views.py
+++ personalwebsite/home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .models import StoryPost
 # Create your views here.
 def homeview(request):
-    return render(request,'landing.html')
+    post=StoryPost.objects.all()
+    arg={'posts':post}
+    return render(request,'template_post.html',arg)
+
+def aboutme(request):
+    return render(request,'aboutme.html')
+
+def stories(request,page):
+    stories_per_page=3;
+    post=StoryPost.objects.order_by('date').all()[(page-1)*stories_per_page:page*stories_per_page]
+    arg={'posts':post}
+    return render(request,'template_stories.html',arg)
