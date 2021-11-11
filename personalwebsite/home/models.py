b4dee939d83be4de51a0a52da1375ba07fb16ccc
reveriess <sayid159@gmail.com>
2021-11-11 23:00:12 +0700

    minor revision

--- personalwebsite/home/models.py
+++ personalwebsite/home/models.py
@@ -2,47 +2,50 @@ from django.db import models
 
 # Create your models here.
 class StoryPost(models.Model):
-    text=models.TextField()
-    date=models.DateTimeField()
-    title=models.CharField(max_length=40)
-    summary=models.TextField()
+    text = models.TextField()
+    date = models.DateTimeField()
+    title = models.CharField(max_length=40)
+    summary = models.TextField()
 
     class Meta:
-        ordering=['date']
+        ordering = ["date"]
 
     def __str__(self):
         return self.title
 
+
 class Education(models.Model):
-    school_name=models.CharField(max_length=40)
-    startdate=models.DateTimeField()
-    enddate=models.DateTimeField()
-    description=models.TextField()
+    school_name = models.CharField(max_length=40)
+    startdate = models.DateField()
+    enddate = models.DateField()
+    description = models.TextField()
 
     class Meta:
-        ordering=['startdate']
+        ordering = ["startdate"]
 
     def __str__(self):
         return self.school_name
 
+
 class Experiences(models.Model):
-    exp_name=models.CharField(max_length=40)
-    startdate=models.DateTimeField()
-    enddate=models.DateTimeField()
-    description=models.TextField()
+    exp_name = models.CharField(max_length=40)
+    startdate = models.DateField()
+    enddate = models.DateField()
+    description = models.TextField()
 
     class Meta:
-        ordering=['startdate']
+        ordering = ["startdate"]
 
     def __str__(self):
         return self.exp_name
 
+
 class CurrentInterests(models.Model):
-    interest_name=models.CharField(max_length=40)
-    description=models.TextField()
+    interest_name = models.CharField(max_length=40)
+    description = models.TextField()
 
     class Meta:
-        ordering=['interest_name']
+        ordering = ["interest_name"]
 
     def __str__(self):
         return self.interest_name
