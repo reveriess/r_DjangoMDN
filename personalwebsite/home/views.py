76afa5bcaa576b1a86d219e5f7fa4174b58909d8
reveriess <sayid159@gmail.com>
2021-11-17 21:32:46 +0700

    refactor stories page, minor UI tweak

--- personalwebsite/home/views.py
+++ personalwebsite/home/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from .models import StoryPost,Education,Experiences,CurrentInterests
+from .models import StoryPost, Education, Experiences, CurrentInterests
+
 # Create your views here.
 def homeview(request):
-    post=StoryPost.objects.all()
-    arg={'posts':post}
-    return render(request,'template_post.html',arg)
+    post = StoryPost.objects.all()
+    arg = {"posts": post}
+    return render(request, "template_post.html", arg)
+
 
 def aboutme(request):
-    education=Education.objects.all()
-    experience=Experiences.objects.all()
-    currentinterest=CurrentInterests.objects.all()
-    arg={'education':education,'experience':experience,'currentinterest':currentinterest}
-    return render(request,'aboutme.html',arg)
+    education = Education.objects.all()
+    experience = Experiences.objects.all()
+    currentinterest = CurrentInterests.objects.all()
+    arg = {
+        "education": education,
+        "experience": experience,
+        "currentinterest": currentinterest,
+    }
+    return render(request, "aboutme.html", arg)
 
-def stories(request,page):
-    stories_per_page=3;
-    post=StoryPost.objects.order_by('date').all()[(page-1)*stories_per_page:page*stories_per_page]
-    arg={'posts':post}
-    return render(request,'template_stories.html',arg)
+
+def stories(request, page):
+    stories_per_page = 3
+    post = StoryPost.objects.order_by("date").all()[
+        (page - 1) * stories_per_page : page * stories_per_page
+    ]
+    arg = {"posts": post,"show_button":True}
+    return render(request, "template_stories.html", arg)
